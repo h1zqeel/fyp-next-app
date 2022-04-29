3468fa10155f1141ca34e27fda5ec5d55a6e2b42
Kraken-cave <bilalarshad1234@hotmail.com>
2022-04-29 16:48:51 +0500

    HomePage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Header from "../components/Header";
+import Layout from "../components/Layout";
+
 
 export default function Component() {
   const { data: session } = useSession()
@@ -6,15 +9,73 @@ export default function Component() {
     console.log(session.user);
     return (
       <>
-        Signed in as {session.user.email} <br />
+        <Layout signIn = {true} >
+        Signed in as {session.user.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
+        </Layout>
+      
+
+      <style jsx>
+        {
+          `
+          
+          .btn{
+            background-color: lightblue;
+            border-radius: 25px;
+            font-size: 20px;
+            outline: none;
+            border: none;
+            cursor: pointer;
+          }
+
+          .btn:hover{
+            padding: 8px 20px;
+            transition: all .3s  ease-out;
+            background: black;
+            color: white;
+            transition: 250ms;
+          }
+          }
+          `
+        }
+      </style>
       </>
+
     )
   }
+
   return (
     <>
-   
-      <button onClick={() => signIn('google')}>Sign in</button>
+    <Layout  >
+    <button className="btn" onClick={() => signIn('google')}>Sign in</button>
+    </Layout>
+      
+
+      <style jsx>
+        {
+          `
+          
+          .btn{
+            background-color: lightblue;
+            border-radius: 25px;
+            font-size: 20px;
+            outline: none;
+            border: none;
+            cursor: pointer;
+          }
+
+          .btn:hover{
+            padding: 8px 20px;
+            transition: all .3s  ease-out;
+            background: black;
+            color: white;
+            transition: 250ms;
+          }
+          }
+          `
+        }
+      </style>
+      
     </>
   )
 }
