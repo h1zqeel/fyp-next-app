04d171c0b455ab8c777167c529357836da2d283e
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2022-04-30 14:34:10 +0500

    Revert "Add home page"

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,47 +1,20 @@
 import { useSession, signIn, signOut } from "next-auth/react"
-import Header from "../components/Header";
-import Layout from "../components/Layout";
-import Intro from "../components/homepage/Into";
-import { useSpring, animated } from 'react-spring'
 
 export default function Component() {
   const { data: session } = useSession()
-  const slideFromLeft = useSpring({ to: { opacity: 1, transform: 'translateX(0px)' }, from: { opacity: 0, transform: 'translateX(-250px)' } })
-
   if (session) {
+    console.log(session.user);
     return (
       <>
-        <Layout session={session} >
-          <animated.div style={slideFromLeft}>
-            <Intro session={session}/>
-          </animated.div>
-        </Layout>
-
-      <style jsx>
-        {
-          `
-          `
-        }
-      </style>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
       </>
-
     )
   }
-
   return (
     <>
-    <Layout>
-      <animated.div style={slideFromLeft}>
-        <Intro session={session}/>
-      </animated.div>
-    </Layout>
-    
-      <style jsx>
-        {
-          `
-          `
-        }
-      </style>
+   
+      <button onClick={() => signIn('google')}>Sign in</button>
     </>
   )
 }
