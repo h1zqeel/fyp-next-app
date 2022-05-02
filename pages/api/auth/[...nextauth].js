41c024be99b63638063aba2d841514d1daf61d4d
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2022-05-02 17:14:20 +0500

    changes for deployment

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,12 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { clientId, clientSecret } from '../../../priv';
 
 export default NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '914783906742-t3ikor5mk2di2ppg0p6nk8m9cnsj6u8g.apps.googleusercontent.com',
-            clientSecret: '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',
+            clientId: process.env.googleclientid || clientId,
+            clientSecret: process.env.googleclientsecret || clientSecret,
           }),
       ],
       options:{
