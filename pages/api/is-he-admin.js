98d2a93f531b4c8e0b0f0df1e14708b95ff8a4ee
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2022-05-03 21:33:11 +0500

    adding admin panel, and its features, incomplete

---
+++ pages/api/is-he-admin.js
@@ -0,0 +1,10 @@
+import prisma from '../../lib/prisma'
+
+export default async function handle(req, res){
+    const result = await prisma.admins.findUnique({where:{email:req.body.email}})
+    if(result)
+        res.json({admin: true})
+    else 
+        res.json({admin: false})
+}
+
