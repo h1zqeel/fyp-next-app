32a3140d9f7045b56ea45074fb8807151d105484
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2022-05-05 17:54:22 +0500

    code cleanup

--- pages/api/admin/add.js
+++ pages/api/admin/add.js
@@ -2,7 +2,7 @@ import prisma from '../../../lib/prisma'
 
 export default async function handle(req, res){
     const email = req.body.email;
-    const result = await prisma.admins.create({data:{email:email}});
+    const result = await prisma.userAdmin.create({data:{email:email}});
     res.json(result);
     
 
--- pages/api/admin/get.js
+++ pages/api/admin/get.js
@@ -1,7 +1,7 @@
 import prisma from '../../../lib/prisma'
 
 export default async function handle(req, res){
-    const result = await prisma.admins.findMany();
+    const result = await prisma.userAdmin.findMany();
     res.json(result);
     
 
