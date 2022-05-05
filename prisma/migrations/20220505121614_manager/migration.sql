fc0335a4c2e414953377f2a0c375bc8c88ca5016
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2022-05-05 17:21:06 +0500

    bug fixes

---
+++ prisma/migrations/20220505121614_manager/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Managers` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Managers_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
