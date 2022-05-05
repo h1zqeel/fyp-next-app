f7747b75e362a13bfc81f5b784114e7386417965
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2022-05-05 17:29:51 +0500

    bug fixes

---
+++ pages/api/admin/isAdmin.js
@@ -0,0 +1,10 @@
+import prisma from '../../lib/prisma'
+
+export default async function handle(req, res){
+    const email = req.body.email;
+    const result = await prisma.managers.findUnique({where:{email:email}})
+    if(result)
+        res.json({admin: true})
+    else
+        res.json({admin: false})
+}
