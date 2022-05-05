cf952c311842cb3cafa0f31c0d89463a0476a477
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2022-05-05 17:41:52 +0500

    admin bug

--- pages/api/admin/isAdmin.js
+++ pages/api/admin/isAdmin.js
@@ -2,7 +2,7 @@ import prisma from '../../../lib/prisma'
 
 export default async function handle(req, res){
     const email = req.body.email;
-    const result = await prisma.managers.findUnique({where:{email:email}})
+    const result = await prisma.userAdmin.findUnique({where:{email:email}})
     if(result)
         res.json({admin: true})
     else
--- pages/api/isHeAdmin.js
+++ pages/api/isHeAdmin.js
@@ -1,7 +1,7 @@
 import prisma from '../../lib/prisma'
 
 export default async function handle(req, res){
-    const result = await prisma.managers.findUnique({where:{email:req.body.email}})
+    const result = await prisma.userAdmin.findUnique({where:{email:req.body.email}})
     if(result)
         res.json({admin: true})
     else
