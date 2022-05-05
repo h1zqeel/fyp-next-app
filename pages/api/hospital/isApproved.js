5d0fc92c84591980c6bd4077d521866cf8d93f78
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2022-05-05 17:21:06 +0500

    bug fixes

--- pages/api/hospital/isApproved.js
+++ pages/api/hospital/isApproved.js
@@ -2,11 +2,11 @@ import prisma from '../../../lib/prisma'
 
 export default async function handle(req, res) {
     const result = await prisma.hospitalRequest.findUnique({where:{email:req.body.email}});
-    if(!result){
-        res.json({approved:'not'})
-        return;
-    }
-    else if(result.approved == 1){
+    // if(!result){
+    //     res.json({approved:'not'})
+    //     return;
+    // }
+    if(result && result.approved == 1){
         res.json({approved:result.approved})
     } else{
         res.json({approved:false})
