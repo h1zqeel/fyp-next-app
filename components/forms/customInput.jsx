4a673077ccbacd0dbe9cac617b7789d3cebb3399
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2022-05-10 19:24:20 +0500

    add mobile view

--- components/forms/customInput.jsx
+++ components/forms/customInput.jsx
@@ -1,7 +1,7 @@
 import { TextField } from "@mui/material";
 
 const CustomInput = ({name,  def='', disabled=false, multiline=false, state, setState}) => {
-    return (<div className="mt-4 w-1/4">
+    return (<div className="mt-4 lg:w-1/4">
                 <TextField id="filled-basic" multiline={multiline} minRows={4} maxRows={10} fullWidth={true} size="small" label={name} variant="outlined" defaultValue={def} disabled={disabled} value={state} onChange={e => setState(e.target.value)}/>
             </div>)
 }
