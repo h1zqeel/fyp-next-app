57a39e617b8cd255ac755d8af495964e6b59080b
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2022-05-16 16:14:55 +0500

    hide sensitive vars

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,12 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { clientId, clientSecret } from '../../../priv';
 
 export default NextAuth({
     secret: process.env.SECRET,
     providers: [
         GoogleProvider({
-            clientId: process.env.googleclientid || clientId,
-            clientSecret: process.env.googleclientsecret || clientSecret,
+            clientId: process.env.googleclientid,
+            clientSecret: process.env.googleclientsecret
           }),
       ],
       options:{
